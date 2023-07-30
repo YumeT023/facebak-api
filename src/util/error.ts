daca1537fa74ac6a3b7f49ade7817a15d3a07192
Yume <hei.hajatiana@gmail.com>
2023-07-30 23:32:05 +0300

    refactor: error struct

--- src/util/error.ts
+++ src/util/error.ts
@@ -1,18 +1,30 @@
-const createError = (statusCode: number, message: string) => {
+export const HttpErrorCodes = {
+  404: "NOT_FOUND",
+  400: "BAD_REQUEST",
+} as const;
+
+export interface HttpError {
+  name: string;
+  statusCode: keyof typeof HttpErrorCodes;
+  message: string;
+}
+
+const createError = (statusCode: keyof typeof HttpErrorCodes, message: string) => {
   return {
     statusCode,
     message,
+    name: HttpErrorCodes[statusCode],
   };
 };
 
 /**
  * not found Error with the provided message
  */
-export function notFoundError(message: string);
+export function notFoundError(message: string): HttpError;
 /**
  * not found Error with the queried resource info 'resource.field=value'
  */
-export function notFoundError(resource: string, field: string, value: string);
+export function notFoundError(resource: string, field: string, value: string): HttpError;
 export function notFoundError(messageOrResource: string, field?: string, value?: string) {
   // ...field argument is provided!, it matches the 2nd overload, with 'resource
   if (field) {
