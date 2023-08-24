cede434bf6c660c55df5d80547f512b11ffde6e5
Yume <hei.hajatiana@gmail.com>
2023-08-25 01:09:20 +0300

    chore(types): make custom hook/decoration type safe

---
+++ src/types.ts
@@ -0,0 +1,10 @@
+import {JWT} from "@fastify/jwt";
+
+declare module "fastify" {
+  interface FastifyRequest {
+    jwt: JWT;
+  }
+  export interface FastifyInstance {
+    authenticate: unknown;
+  }
+}
